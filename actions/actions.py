7c74fda335677844982e8297dbca9884e4861fd6
Richard Mazúr <richard.mazur9@gmail.com>
2023-04-12 08:23:18 +0200

    use relative path when searching in external_data_test

--- actions/actions.py
+++ actions/actions.py
@@ -3,8 +3,7 @@
 #
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
-
-
+import os.path
 # This is a simple example for a custom action which utters "Hello World!"
 
 # from typing import Any, Text, Dict, List
@@ -32,7 +31,10 @@ from datetime import date
 from rasa_sdk.events import SlotSet
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-
+#TODO move to some config file as a constant
+full_path = os.path.realpath(__file__)
+actions_dir = os.path.dirname(full_path)
+project_root_dir = os.path.dirname(actions_dir)
 
 class ActionSaySubscriptionEmail(Action):
 
@@ -59,7 +61,7 @@ class ShowOfficeHoursTime(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        df = pd.read_excel(r"C:\Users\skyselect\Desktop\PROJECTS - Copy\esn_cu_chatbot\external_data\office_hours.xlsx")
+        df = pd.read_excel(os.path.join(project_root_dir, "external_data_test", "office_hours.xlsx"))
         df['date'] = pd.to_datetime(df['date'], format='%d%m%Y')
         df = df.set_index('date')
 
