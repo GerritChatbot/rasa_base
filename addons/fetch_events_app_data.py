8053ccd6466eda10cd9b99bd7a98a805edbac226
essare-rimaz <richard.mazur9@gmail.com>
2024-02-13 20:20:38 +0100

    refactor fetch app data
    - needed to comply with new date format

--- addons/fetch_events_app_data.py
+++ addons/fetch_events_app_data.py
@@ -1,11 +1,12 @@
 from gql import Client, gql
 from gql.transport.aiohttp import AIOHTTPTransport
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 # https://pypi.org/project/jq/
 # https://pypi.org/project/pyjq/
 import json
 import os.path as path
 from dateutil.parser import parse
+import pytz
 
 # Select your transport with a defined url endpoint
 header = {'x-tumi-tenant': 'cu-prague'}
@@ -14,15 +15,19 @@ transport = AIOHTTPTransport(url="https://cu-prague.esn.world/graphql", headers=
 # Create a GraphQL client using the defined transport
 client = Client(transport=transport, fetch_schema_from_transport=True)
 
-today = datetime.now().date().strftime("%Y.%m.%d %H:%M")
-within_two_weeks = parse(today) + timedelta(weeks=3)
+prague_tz = pytz.timezone('Europe/Prague')
+now = datetime.now(prague_tz)
+today = now.replace(minute=0, second=0, microsecond=0)
+today_iso = today.isoformat()
+within_two_weeks = today + timedelta(weeks=3)
+within_two_weeks_iso = within_two_weeks.isoformat()
 # Provide a GraphQL query
 # after can also handle hours
 query = gql(
     f"""
     query MyQuery {{
       events(
-        after: "{today}", before: "{within_two_weeks}"
+        after: "{today_iso}", before: "{within_two_weeks_iso}"
       ) {{
         id
         title
